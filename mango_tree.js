2d234f46d6d96e6d7eaf08a1811df6e3b9715fdc
susiantiamelia <ameliasusianti@gmail.com>
2018-06-04 22:47:32 +0700

    Done

--- mango_tree.js
+++ mango_tree.js
@@ -5,30 +5,46 @@
 class MangoTree {
 
   // Initialize a new MangoTree
-  constructor () {
+  constructor (age=0, height= 0, fruits=[], healthStatus='good', harvested=0) {
+    this._age = age,
+    this._height = height,
+    this._fruits = fruits,
+    this._healthStatus = healthStatus,
+    this._harvested = harvested
   }
 
   get age () {
+    return this._age
   }
 
   get height () {
-
+    return this._height
   }
 
   get fruits () {
+    return this._fruits
   }
 
   get healthStatus () {
+    return this._healthStatus
   }
 
   get harvested () {
-
+    return this._harvested
   }
 
   // Get current states here
 
   // Grow the tree
   grow () {
+    this._age++;
+
+    if (this._age < 20) {
+      this._height += (Math.round(Math.floor()*100) / 100) + ' m' ;
+      return this._healthy = true;
+    } else {
+      return this._healthy = false;
+    }
   }
 
   // Produce some mangoes
@@ -44,6 +60,7 @@ class MangoTree {
 class Mango {
   // Produce a mango
   constructor () {
+    this.quality= 
   }
 }
 
