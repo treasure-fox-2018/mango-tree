39d1d02da77c005a5be8263da7e6d84a3b457990
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-06-04 22:14:06 +0700

    finish release 0 and partially finish release 1

--- mango_tree.js
+++ mango_tree.js
@@ -5,30 +5,44 @@
 class MangoTree {
 
   // Initialize a new MangoTree
-  constructor () {
+  constructor (age, height, fruitsAmount, pickedFruitsAmount, healthStatus) {
+    this._age = age;
+    this._height = height;
+    this._fruitsAmount = fruitsAmount;
+    this._pickedFruitsAmount = pickedFruitsAmount;
+    this._healthStatus = healthStatus;
   }
 
   get age () {
+    return this._age;
   }
 
   get height () {
-
+    return this._height;
   }
 
   get fruits () {
+    return this._fruitsAmount;
   }
 
   get healthStatus () {
+    return this._healthStatus;
   }
 
   get harvested () {
-
+    return this._pickedFruitsAmount;
   }
 
   // Get current states here
 
   // Grow the tree
   grow () {
+    this._age = this._age + 1;
+    this._height = this._height + (Math.trunc(Math.random() * 5));
+
+    var matureAge = 5;
+    var stopHeightAge = 10;
+    var stopFruitAge = 15;
   }
 
   // Produce some mangoes
@@ -44,6 +58,8 @@ class MangoTree {
 class Mango {
   // Produce a mango
   constructor () {
+    var arrStr = ["good", "bad"];
+    this._quality = arrStr[Math.trunc(Math.random() * arrStr.length)];
   }
 }
 
