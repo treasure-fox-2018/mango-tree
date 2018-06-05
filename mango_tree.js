1930b9f61a0ac9f6dedbbe945ff0bea1a6c91fec
David Yunus <dove.fit@gmail.comgit config --global user.name>
2018-06-05 07:37:59 +0700

    update until 1st release

--- mango_tree.js
+++ mango_tree.js
@@ -1,67 +1,168 @@
 "use strict"
 
-// Release 0
+// release 0
 
 class MangoTree {
-
   // Initialize a new MangoTree
-  constructor () {
+  constructor() {
+    this._age = 0;
+    this._height = 0;
+    this._fruits = [];
+    this._healthyStatus = 'alive';
+    this._harvested = ''
   }
 
-  get age () {
+  get age() {
+    return this._age;
   }
 
-  get height () {
-
+  get height() {
+    return this._height;
   }
 
-  get fruits () {
+  get fruits() {
+    return this._fruits;
   }
 
-  get healthStatus () {
+  get healtyStatus() {
+    return this._healthyStatus;
   }
 
-  get harvested () {
-
+  get harvested() {
+    return this._harvested;
   }
+
 
   // Get current states here
 
   // Grow the tree
-  grow () {
+  grow() {
+      let randomNum = Math.random().toFixed(1);
+      this._age++;
+      if(this._age < 11) {
+        this._height += Number(randomNum);
+        this._height = Number(this._height.toFixed(1));
+      }
+
+      if(this._age >= 20) {
+        this._healthyStatus = 'dead';
+      }
   }
 
   // Produce some mangoes
-  produceMangoes () {
+  produceMangoes() {
+    if(this._age > 4) {
+      let randomNum = Math.ceil(Math.random() * 15);
+      for(let i = 0; i < randomNum; i++) {
+        this._fruits.push(new Mango())
+      }
+    }
   }
 
   // Get some fruits
-  harvest () {
+  harvest() {
+    let good = 0;
+    let bad = 0;
+    for(let i = 0; i < this._fruits.length; i++) {
+      if(this._fruits[i].quality == 'good') {
+        good++
+      } else {
+        bad++
+      }
+    }
   }
 
 }
 
 class Mango {
   // Produce a mango
-  constructor () {
+  constructor() {
+    this.quality =  this.fruitQuality();
+  }
+
+  fruitQuality() {
+    let random = Math.round(Math.random());
+    if(random == 0) {
+      return 'good';
+    } else {
+      return 'bad';
+    }
   }
 }
 
-/**
-  * driver code untuk release 0
-  * let mangoTree = new MangoTree()
-  * do {
-  *   mangoTree.grow();
-  *   mangoTree.produceMangoes();
-  *   mangoTree.harverst();
-  *   console.log(`[Year ${tree.age} Report] Height = ${tree.height} | Fruits harvested = ${tree.harvested}`)
-  * } while (mangoTree.healthStatus != false)
-  */
+  // driver code untuk release 0
+  let mangoTree = new MangoTree()
+  do {
+
+    if(mangoTree.age == 0) {
+      console.log(`The ${mangoTree.name} tree is alive :smile:`)
+    }
+
+    mangoTree.grow();
+    mangoTree.produceMangoes();
+    mangoTree.harvest();
+    console.log(`[Year ${mangoTree.age} Report] Height = ${mangoTree.height} | Fruits harvested = ${mangoTree.harvested}`);
+    
+    if(mangoTree.age >= 20) {
+      console.log(`The ${mangoTree.name} tree is no more :sad:`);
+    }
+
+  } while (mangoTree._healthyStatus != 'dead')
+  
+
 
 // Release 1
-class AppleTree {}
-class Apple {}
+class AppleTree extends MangoTree {
+  constructor(name) {
+    super(name);
+  }
+
+  grow() {
+    let random = Math.random().toFixed(1);
+    this._age++;
+    if(this._age < 6) {
+      this._height += Number(random);
+      this._height = Number(this._height.toFixed(1));
+    }
+    if(this._age >= 15  ) {
+      this._healthyStatus = 'dead';
+    }
+  }
+
+  produceApple() {
+    if(this._age >= 7) {
+      let random = Math.ceil(Math.random() * 30);
+    
+      for(let i = 0; i < random; i++) {
+        this._fruits.push(new Mango);
+      }
+    }
+  }
+}
+class Apple extends Mango{
+  constructor() {
+    super();
+  }
+}
+
+let appleTree = new AppleTree('Apple');
+do {
+  if (appleTree.age === 0) {
+    console.log(`The ${appleTree.name} tree is alive :smile:`);
+  }
+  appleTree.grow();
+  appleTree.produceApple();
+  appleTree.harvest();
+  console.log(`[Year ${appleTree.age} Report] Height = ${appleTree.height}m | Fruits harvested = ${appleTree.harvested}`);
+  if (appleTree.age >= 15) {
+    console.log(`The ${appleTree.name} tree is no more :sad:`);
+  }
+} while (appleTree._healthyStatus != 'dead');
 
 // Release 2
-class FruitTree {}
-class Fruit {}
+class FruitTree {
+
+}
+class Fruit {
+
+}
